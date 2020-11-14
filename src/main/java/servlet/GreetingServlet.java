880f093cb436af29786a4de5f2c3cbdeb29209a5
ogneyar79 <maksimus.s@mail.ru>
2020-11-14 10:55:40 +0300

    Add sending Json at GreetingServlet

--- src/main/java/servlet/GreetingServlet.java
+++ src/main/java/servlet/GreetingServlet.java
@@ -1,22 +1,33 @@
 package servlet;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
-public class GreetingServlet extends HttpServlet{
+public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ArrayList<String> list = new ArrayList<>();
+
+        Gson gson = new Gson();
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
         String name = req.getParameter("name");
+
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         resp.setHeader("Access-Control-Allow-Origin", "*");
-        writer.println("Nice to meet you, " + name);
+        list.add(name);
+        list.add("Nice to meet you, ");
+        String js = gson.toJson(list);
+        System.out.println(js + "GreetingServlet doGet");
+        writer.println(js);
         writer.flush();
     }
 }
